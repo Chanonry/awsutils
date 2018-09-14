5626daaee90f3665324673ec4c246b8d83ba19f3
Chanonry <andy.spence@me.com>
2018-09-14 15:12:59 +0100

    0.1.1 package discovery

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
-    name='lambda_helpers',
-    version='0.1.0',
+setup(
+    name='helpers',
+    version='0.1.1',
     author="Andy Spence",
     author_email="andy.spence@icloud.com",
     description="Helper fn's for aws s3 and SQS",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=[],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python',
