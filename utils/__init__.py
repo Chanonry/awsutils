ec2e3f32fc2bfc5847f36aef32a45bdf80f0b153
Chanonry <andy.spence@me.com>
2018-09-14 17:10:57 +0100

    renamed awsutils

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from utils.filesystem import bucket_access, key_access, download, upload, move, move_core
-from utils.sqs import decode_b64, get_queue, send_message, process_msg, del_message, purge_sqs
-
-__version__ = '0.1.1'
-__author__ = 'Andy Spence <andy.spence@icloud.com>'
-__all__ = []
