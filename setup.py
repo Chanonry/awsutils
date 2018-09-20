9f79a23bb94bba119235684b75a68b446000f06e
Chanonry <andy.spence@me.com>
2018-09-20 18:42:33 +0100

    0.1.2 get_msg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='awsutils',
-    version='0.1.1',
+    version='0.1.2',
     author="Andy Spence",
     author_email="",
     description="Helper fn's for aws s3 and SQS",
