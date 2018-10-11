4ac6cea85d7d1f4f1fbce24e6d4a20c9a73f43af
Chanonry <andy.spence@me.com>
2018-10-11 14:07:52 +0100

    1.0.1 sources root change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='awsutils',
-    version='1.0.0',
+    version='1.0.1',
     author="Andy Spence",
     author_email="",
     description="Helper fn's for aws s3 and SQS",
