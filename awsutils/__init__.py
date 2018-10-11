bc92f2ca5398c3b37f691a24e21f0896bbdf6441
Chanonry <andy.spence@me.com>
2018-10-11 14:25:52 +0100

    revert to 21/09 changes

--- awsutils/__init__.py
+++ awsutils/__init__.py
@@ -1,7 +1,6 @@
-from awsutils.filesystem import bucket_access, key_access, key_metadata, download, upload, move, move_core
-from awsutils.filesystem import s3_bucket_access, s3_key_access, s3_download, s3_upload, s3_move, remove_return_value
-from awsutils.sqs import decode_b64, get_queue, send_message, get_msg, del_message, purge_sqs, msg_test
+from awsutils.filesystem import bucket_access, key_access, download, upload, move, move_core
+from awsutils.sqs import decode_b64, get_queue, send_message, get_msg, del_message, purge_sqs
 
-__version__ = '1.0.1'
+__version__ = '0.1.3'
 __author__ = 'Andy Spence <andy.spence@icloud.com>'
 __all__ = []
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='awsutils',
-    version='1.0.1',
+    version='0.1.2',
     author="Andy Spence",
     author_email="",
     description="Helper fn's for aws s3 and SQS",
     packages=find_packages(),
     install_requires=[],
     classifiers=[
-        'Development Status :: Beta',
+        'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
